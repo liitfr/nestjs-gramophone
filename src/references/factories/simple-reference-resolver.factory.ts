d14d67edd29ffddd2d6871fa0a48c8b496a2b068
soolidtech <hello@soolid.tech>
2023-04-02 17:48:34 +0200

    feat: simple reference resolvers

--- src/references/factories/simple-reference-resolver.factory.ts
+++ src/references/factories/simple-reference-resolver.factory.ts
@@ -3,57 +3,102 @@ import { Query, Resolver } from '@nestjs/graphql';
 
 import { SimpleService } from '../../utils/services/simple.service';
 import { SimpleResolverFactory } from '../../utils/resolvers/simple-resolver.factory';
-import { getEntityMetadata } from '../../utils/entities/entity.util';
 import { pascalCase, pluralize } from '../../utils/string.util';
 import { getResolverMetadata } from '../../utils/resolvers/resolver.util';
 import { getServiceMetadata } from '../../utils/services/service.util';
+import { getReferenceMetadata } from '../../utils/references/reference.util';
 
 interface Options {
   noMutation?: boolean;
+  noPartition?: boolean;
 }
 
+const defaultOptions: Options = {
+  noMutation: true,
+  noPartition: true,
+};
+
 export function SimpleReferenceResolverFactory<D>(
-  Entity: Type<unknown>,
+  Reference: Type<unknown>,
   Input: Type<unknown>,
   Service: Type<SimpleService<D>>,
-  options: Options = {
-    noMutation: true,
-  },
+  pOptions: Options = defaultOptions,
 ) {
+  const options = { ...defaultOptions, ...pOptions };
+
   const SimpleResolver = SimpleResolverFactory<D>(
-    Entity,
+    Reference,
     Input,
     Service,
     options,
   );
 
-  const entityMetadata = getEntityMetadata(Entity);
+  const referenceMetadata = getReferenceMetadata(Reference);
   const resolverMetadata = getResolverMetadata(SimpleResolver);
   const serviceMetadata = getServiceMetadata(Service);
 
-  const queryName = `getAllActive${pascalCase(
-    pluralize(entityMetadata.entityName),
-  )}`;
+  const { referenceName, referenceDescription, referencePartitioner } =
+    referenceMetadata;
 
-  @Resolver(() => Entity)
+  const pCPName = pascalCase(pluralize(referenceName));
+  const findAllQueryName = `findAllActive${pCPName}`;
+
+  @Resolver(() => Reference)
   class SimpleReferenceResolver extends SimpleResolver {
-    @Query(() => [Entity], { name: queryName })
-    async [queryName]() {
-      if (!this.simpleService[queryName]) {
+    @Query(() => [Reference], {
+      name: findAllQueryName,
+      nullable: false,
+      description: `${referenceName} : Find all active query`,
+    })
+    async [findAllQueryName]() {
+      if (!this.simpleService[findAllQueryName]) {
         throw new Error(
           'The service ' +
             serviceMetadata.serviceName +
             ' does not implement the method ' +
-            queryName,
+            findAllQueryName,
         );
       }
-      return this.simpleService[queryName]();
+      return this.simpleService[findAllQueryName]();
     }
   }
 
   Object.defineProperty(SimpleReferenceResolver, 'name', {
     value: resolverMetadata.resolverName,
   });
+
+  if (!options.noPartition) {
+    Object.entries(referencePartitioner).forEach(([key]) => {
+      const pCKey = pascalCase(key);
+      const serviceMethodName = `find${pCKey}`;
+      const queryName = `find${referenceName}${pCKey}`;
+
+      SimpleReferenceResolver.prototype[queryName] = async function () {
+        if (!this.simpleService[serviceMethodName]) {
+          throw new Error(
+            'The service ' +
+              serviceMetadata.serviceName +
+              ' does not implement the method ' +
+              serviceMethodName,
+          );
+        }
+        return (await this.simpleService[serviceMethodName]?.())?.[0];
+      };
+
+      Query(() => Reference, {
+        name: queryName,
+        nullable: true,
+        description: `${referenceDescription} : Find ${key} query`,
+      })(
+        SimpleReferenceResolver.prototype,
+        queryName,
+        Object.getOwnPropertyDescriptor(
+          SimpleReferenceResolver.prototype,
+          queryName,
+        ),
+      );
+    });
+  }
 
   return SimpleReferenceResolver;
 }
