7636d1d3a24fb0224e79a68e43370b62ff8e2aff
soolidtech <hello@soolid.tech>
2023-04-02 07:16:17 +0200

    fix: SimpleReferenceModelFactory usage

--- src/references/models/color.model.ts
+++ src/references/models/color.model.ts
@@ -1,17 +1,12 @@
-import { Type as NestType } from '@nestjs/common';
-
 import { HydratedDocument } from 'mongoose';
 
 import { ColorEnum } from '../enums/color.enum';
 
 import { SimpleReferenceModelFactory } from './simple-reference-model.factory';
 
-const { SimpleReference, SimpleReferenceSchema } = SimpleReferenceModelFactory(
-  ColorEnum,
-  'Color',
-);
+const { SimpleReference: Color, SimpleReferenceSchema: ColorSchema } =
+  SimpleReferenceModelFactory(ColorEnum, 'Color');
 
-export type ColorDocument = HydratedDocument<NestType>;
+export type ColorDocument = HydratedDocument<typeof Color>;
 
-export { SimpleReference as Color };
-export { SimpleReferenceSchema as ColorSchema };
+export { Color, ColorSchema };
--- src/references/models/type.model.ts
+++ src/references/models/type.model.ts
@@ -1,17 +1,12 @@
-import { Type as NestType } from '@nestjs/common';
-
 import { HydratedDocument } from 'mongoose';
 
 import { TypeEnum } from '../enums/type.enum';
 
 import { SimpleReferenceModelFactory } from './simple-reference-model.factory';
 
-const { SimpleReference, SimpleReferenceSchema } = SimpleReferenceModelFactory(
-  TypeEnum,
-  'Type',
-);
+const { SimpleReference: Type, SimpleReferenceSchema: TypeSchema } =
+  SimpleReferenceModelFactory(TypeEnum, 'Type');
 
-export type TypeDocument = HydratedDocument<NestType>;
+export type TypeDocument = HydratedDocument<typeof Type>;
 
-export { SimpleReference as Type };
-export { SimpleReferenceSchema as TypeSchema };
+export { Type, TypeSchema };
